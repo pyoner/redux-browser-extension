870e84bc5caf934f8f8a01187bf92fb95e7d9545
Jungle <devex.soft@gmail.com>
2016-08-23 16:44:40 +0500

    added tasks clean, copy-assets, rollup to gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,14 @@ var path = require('path');
 var gulp = require('gulp');
 var rollup = require('rollup-stream');
 var source = require('vinyl-source-stream');
+var gulpSequence = require('gulp-sequence');
+var del = require('del');
 var config = require('./config');
+
+
+var BUILD_DIR = './build';
+var BUILD_SRC_DIR = path.join(BUILD_DIR, 'src');
+var BUILD_ASSETS_DIR = path.join(BUILD_DIR, 'assets');
 
 function rollit(src, dest) {
     var opts = Object.assign({}, { entry: src }, config);
@@ -12,19 +19,29 @@ function rollit(src, dest) {
         .pipe(gulp.dest(dest));
 }
 
-gulp.task('build', function() {
+gulp.task('clean', function(){
+    return del(BUILD_DIR);
+});
+
+gulp.task('copy-assets', function(){
+    gulp.src('./assets/**')
+    .pipe(gulp.dest(BUILD_ASSETS_DIR))
+});
+
+gulp.task('rollup', function() {
     var entries = [
         './src/background.js',
         './src/content.js',
     ];
-    var dest = './build';
 
     var promises = entries.map(function(entry) {
         return new Promise(function(resolve, reject) {
-            var stream = rollit(entry, dest);
+            var stream = rollit(entry, BUILD_SRC_DIR);
             stream.on('end', resolve);
             stream.on('error', reject);
         })
     })
     return Promise.all(promises);
 });
+
+gulp.task('build', gulpSequence('clean', ['copy-assets', 'rollup']));
