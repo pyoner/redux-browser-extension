2dd3e4514cbd6b24a38388ce0621306133038227
Jungle <devex.soft@gmail.com>
2017-10-17 10:42:31 +0500

    Fix connectToStore

--- src/connect-to-store.js
+++ src/connect-to-store.js
@@ -1,13 +1,15 @@
-export default function connectToStore(callback) {
-    let subscribers = [];
-    let uid = Math.random().toString().slice(2);
-    let port = chrome.runtime.connect(null, { name: uid });
+import uuid from 'uuid';
+
+export default function connectToStore(callback, opts = {}) {
+    const { portName = uuid(), localKey } = opts;
+    const subscribers = [];
+    const port = chrome.runtime.connect(null, { name: portName });
     port.onMessage.addListener((message) => {
         switch (message.type) {
             case 'MSG_PORT_INIT_STORE':
                 store.state = message.payload;
                 if (callback) {
-                    callback(port, store, message.meta);
+                    callback(port, store, {...message.meta, localKey });
                     callback = null;
                 }
                 break;
@@ -18,23 +20,27 @@ export default function connectToStore(callback) {
         }
     });
 
-    let store = {
+    const store = {
         state: null,
         getState() {
             return this.state;
         },
 
         dispatch(action) {
+            const meta = { localKey, ...action.meta }
             port.postMessage({
                 type: 'MSG_REDUX_DISPATCH',
-                payload: action
+                payload: {
+                    ...action,
+                    meta
+                }
             });
         },
 
         subscribe(handler) {
             subscribers.push(handler);
             return function() {
-                let i = subscribers.indexOf(handler);
+                const i = subscribers.indexOf(handler);
                 subscribers.splice(i, 1);
             }
         },
