6f3983e42c5bcaf2440d347ebda96e18c98a7f0c
Jungle <devex.soft@gmail.com>
2017-10-17 10:38:48 +0500

    Fix webpack config plugins (Bump, Uglify), modules config

--- webpack.config.js
+++ webpack.config.js
@@ -6,35 +6,61 @@ var Bump = require("bump-webpack-plugin");
 
 
 var BUILD_DIR = path.join(__dirname, './build');
-var BUILD_SRC_DIR = path.join(BUILD_DIR, 'src');
+var SRC_DIR = path.join(__dirname, 'src');
 
 var config = {
     entry: {
         background: './src/background.js',
-        content: './src/content.js',
+        content: './src/content.js'
     },
     output: {
         filename: '[name].js',
-        path: BUILD_SRC_DIR,
+        path: BUILD_DIR,
     },
     resolveLoader: {
         root: path.join(__dirname, 'node_modules'),
     },
+    node: {
+        fs: 'empty',
+    },
     module: {
         loaders: [{
-            test: /\.jsx?$/,
-            loader: 'babel-loader',
-            include: path.join(__dirname, 'src'),
-            query: {
-                presets: [
-                    'es2015',
-                ]
+                test: /\.css$/,
+                loader: 'css-loader',
+            },
+            {
+                test: /\.(jpe?g|png|ttf|eot|svg|woff(2)?)(\?[a-z0-9=&.]+)?$/,
+                loader: 'base64-inline-loader',
+                query: {
+                    name: '[name].[ext]',
+                }
+            },
+            {
+                test: /\.json$/,
+                loader: 'json-loader',
+                //include: SRC_DIR,
+            },
+            {
+                test: /\.jsx?$/,
+                loader: 'babel-loader',
+                include: SRC_DIR,
+                query: {
+                    presets: [
+                        'es2015',
+                        'stage-1',
+                        'react',
+                    ],
+                    plugins: [
+                        'transform-unicode-property-regex',
+                        'transform-runtime',
+                        'closure-elimination',
+                    ]
+                }
             }
-        }]
+        ]
     },
     plugins: [
         new RemoveWebpackPlugin(['./build']),
-        new Bump(['package.json', 'manifest.json']),
         new CopyWepbackPlugin([
             { from: './assets/**', to: BUILD_DIR },
             { from: 'manifest.json', to: BUILD_DIR }
@@ -46,8 +72,19 @@ if (process.env.NODE_ENV == 'production') {
         new webpack.DefinePlugin({
             'process.env': { 'NODE_ENV': JSON.stringify('production') }
         }),
-        new webpack.optimize.UglifyJsPlugin({ compress: { warnings: true } })
+        new webpack.optimize.UglifyJsPlugin({
+            compress: {
+                warnings: false,
+            },
+            output: {
+                ascii_only: true,
+            },
+        })
     ])
+} else {
+    config.plugins = [
+        new Bump(['package.json', 'manifest.json']),
+    ].concat(config.plugins)
 }
 
 module.exports = config;
